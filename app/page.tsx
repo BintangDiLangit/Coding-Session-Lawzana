076b2d8ed42bcfb7833bc7d1bbca7a3db5e5e239
Pokkrong (Moo) Jhingjit <pokkrong@webquest.io>
2024-09-11 00:15:02 +0700

    Refactor CSS and add Tailwind CSS forms plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,327 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { Radio, RadioGroup } from "@headlessui/react";
+import { CheckCircleIcon } from "@heroicons/react/20/solid";
+
+const deliveryMethods = [
+  {
+    id: 1,
+    title: "Standard",
+    turnaround: "4–10 business days",
+    price: "$5.00",
+  },
+  { id: 2, title: "Express", turnaround: "2–5 business days", price: "$16.00" },
+];
+const paymentMethods = [
+  { id: "credit-card", title: "Credit card" },
+  { id: "paypal", title: "PayPal" },
+  { id: "etransfer", title: "eTransfer" },
+];
+
+const steps = [
+  { id: "Step 1", name: "Contact information", href: "#", status: "current" },
+  {
+    id: "Step 2",
+    name: "Shipping information",
+    href: "#",
+    status: "incomplete",
+  },
+  { id: "Step 3", name: "Delivery method", href: "#", status: "incomplete" },
+  { id: "Step 4", name: "Payment", href: "#", status: "incomplete" },
+];
+
+export default function CheckoutForm() {
+  const selectedDeliveryMethod = deliveryMethods[0];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-gray-50">
+      <div className="mx-auto max-w-4xl px-4 pb-24 pt-16 sm:px-6  lg:px-8">
+        <nav aria-label="Progress">
+          <ol className="space-y-4 md:flex md:space-x-8 md:space-y-0">
+            {steps.map((step) => (
+              <li key={step.name} className="md:flex-1">
+                {step.status === "complete" ? (
+                  <a
+                    href={step.href}
+                    className="group flex flex-col border-l-4 border-orange-600 py-2 pl-4 hover:border-orange-800 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
+                  >
+                    <span className="text-sm font-medium text-orange-600 group-hover:text-orange-800">
+                      {step.id} {step.name}
+                    </span>
+                  </a>
+                ) : step.status === "current" ? (
+                  <a
+                    href={step.href}
+                    aria-current="step"
+                    className="flex flex-col border-l-4 border-orange-600 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
+                  >
+                    <span className="text-sm font-medium text-orange-600">
+                      {step.id} {step.name}
+                    </span>
+                  </a>
+                ) : (
+                  <a
+                    href={step.href}
+                    className="group flex flex-col border-l-4 border-gray-200 py-2 pl-4 hover:border-gray-300 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
+                  >
+                    <span className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+                      {step.id} {step.name}
+                    </span>
+                  </a>
+                )}
+              </li>
+            ))}
+          </ol>
+        </nav>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <form className="lg:grid mt-8">
+          <div>
+            <div>
+              <h2 className="text-lg font-medium text-gray-900">
+                Contact information
+              </h2>
+
+              <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+                <div className="">
+                  <label
+                    htmlFor="email-address"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email address
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="email-address"
+                      name="email-address"
+                      type="email"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div className="">
+                  <label
+                    htmlFor="phone"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Phone
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="phone"
+                      name="phone"
+                      type="text"
+                      autoComplete="tel"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-span-2 gap-2">
+                  <button
+                    type="button"
+                    disabled
+                    className="rounded bg-gray-300 px-2 py-2.5 text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300 w-full"
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-10 border-t border-gray-200 pt-10">
+              <h2 className="text-lg font-medium text-gray-900">
+                Shipping information
+              </h2>
+
+              <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+                <div>
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Full name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="first-name"
+                      name="first-name"
+                      type="text"
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="address"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Address
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="address"
+                      name="address"
+                      type="text"
+                      autoComplete="street-address"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="city"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    City
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="city"
+                      name="city"
+                      type="text"
+                      autoComplete="address-level2"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="postal-code"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Postal code
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="postal-code"
+                      name="postal-code"
+                      type="text"
+                      autoComplete="postal-code"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-10 border-t border-gray-200 pt-10">
+              <fieldset>
+                <legend className="text-lg font-medium text-gray-900">
+                  Delivery method
+                </legend>
+                <RadioGroup
+                  value={selectedDeliveryMethod}
+                  onChange={() => {}}
+                  className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4"
+                >
+                  {deliveryMethods.map((deliveryMethod) => (
+                    <Radio
+                      key={deliveryMethod.id}
+                      value={deliveryMethod}
+                      aria-label={deliveryMethod.title}
+                      aria-description={`${deliveryMethod.turnaround} for ${deliveryMethod.price}`}
+                      className="group relative flex cursor-pointer rounded-lg border border-gray-300 bg-white p-4 shadow-sm focus:outline-none data-[checked]:border-transparent data-[focus]:ring-2 data-[focus]:ring-orange-500"
+                    >
+                      <span className="flex flex-1">
+                        <span className="flex flex-col">
+                          <span className="block text-sm font-medium text-gray-900">
+                            {deliveryMethod.title}
+                          </span>
+                          <span className="mt-1 flex items-center text-sm text-gray-500">
+                            {deliveryMethod.turnaround}
+                          </span>
+                          <span className="mt-6 text-sm font-medium text-gray-900">
+                            {deliveryMethod.price}
+                          </span>
+                        </span>
+                      </span>
+                      <CheckCircleIcon
+                        aria-hidden="true"
+                        className="h-5 w-5 text-orange-600 [.group:not([data-checked])_&]:hidden"
+                      />
+                      <span
+                        aria-hidden="true"
+                        className="pointer-events-none absolute -inset-px rounded-lg border-2 border-transparent group-data-[focus]:border group-data-[checked]:border-orange-500"
+                      />
+                    </Radio>
+                  ))}
+                </RadioGroup>
+              </fieldset>
+            </div>
+
+            {/* Payment */}
+            <div className="mt-10 border-t border-gray-200 pt-10">
+              <h2 className="text-lg font-medium text-gray-900">Payment</h2>
+
+              <fieldset className="mt-4">
+                <legend className="sr-only">Payment type</legend>
+                <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+                  {paymentMethods.map((paymentMethod, paymentMethodIdx) => (
+                    <div key={paymentMethod.id} className="flex items-center">
+                      {paymentMethodIdx === 0 ? (
+                        <input
+                          defaultChecked
+                          id={paymentMethod.id}
+                          name="payment-type"
+                          type="radio"
+                          className="h-4 w-4 border-gray-300 text-orange-600 focus:ring-orange-500"
+                        />
+                      ) : (
+                        <input
+                          id={paymentMethod.id}
+                          name="payment-type"
+                          type="radio"
+                          className="h-4 w-4 border-gray-300 text-orange-600 focus:ring-orange-500"
+                        />
+                      )}
+
+                      <label
+                        htmlFor={paymentMethod.id}
+                        className="ml-3 block text-sm font-medium text-gray-700"
+                      >
+                        {paymentMethod.title}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </fieldset>
+
+              <div className="mt-6 grid grid-cols-4 gap-x-4 gap-y-6">
+                <div className="col-span-4">
+                  <label
+                    htmlFor="name-on-card"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Name on card
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="name-on-card"
+                      name="name-on-card"
+                      type="text"
+                      autoComplete="cc-name"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div className="col-span-4">
+                  <button
+                    type="button"
+                    className="rounded bg-orange-600 px-2 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 w-full"
+                  >
+                    Confirm
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
